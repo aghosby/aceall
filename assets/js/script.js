279ae2ff1032e3bef8e668f84ad0f05572f72c63
aghosby <53856186+aghosby@users.noreply.github.com>
2021-08-09 11:50:30 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,97 @@
+// JavaScript Document
+
 $(document).ready(function() {
-	"use strict";
 
-	$('.header').scrollToFixed({
-		zIndex: 7
+	var d = $('.header');
+	d.scrollToFixed({
+		zIndex: 999
 	});
 
-    $('.flexslider').flexslider({
-		animation: "fade",
-		controlNav: true,
-		animationSpeed: 3000,
-		directionNav: true
-	});
-
-	/*$(window).scroll(function(){ 
-        if ($(this).scrollTop() > 1200) { 
-            $('#scroll').fadeIn(1000); 
-        } else { 
-            $('#scroll').fadeOut(1000); 
-        } 
-	});
-	*/
-
-
-	$(".menu_icon").click(function(e){
-		e.preventDefault();
-		$(".menu").fadeToggle(700);
-		$(".menu").toggleClass("open");
-	});
-	
-	
 	$(window).scroll(function () {
-		var scroll = $(window).scrollTop();
-		
-		var d = $('.header');
-		if (scroll >= 70) d.addClass('smheader');
-		else d.removeClass('smheader');
+		scroll = $(window).scrollTop();
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			if (scroll >= 50) d.addClass('smheader');
+			else d.removeClass('smheader');
+		} else {
+			if (scroll >= 80) d.addClass('smheader');
+			else d.removeClass('smheader');
+	    }	
 	});
+
 
 	$("body").delegate(".ic", "click", function (e) {
 		e.stopPropagation();
-		var c = $(this).parents(".car_hld"),
-			m = c.find('.car_det');
-		$(".car_hld .car_det").not(m).slideUp(1000);
+		var c = $(this).parents(".feedbk-hld"),
+			m = c.find('.details');
+		$(".feedbk .details").not(m).slideUp(1000);
 		m.slideToggle(1000);
+		$(this).find(".fa-chevron-down").toggleClass('sh');
+	});
+
+
+	$('.counter').counterUp({
+		delay: 10,
+		time: 10000
+	});
+    
+
+	$(".menu_icon").click(function(e){
+		e.preventDefault();
+        $(".menu").animate({
+            height: "toggle"
+    });
+	($(".menu_text").text() === "CLOSE") ? $(".menu_text").text("MENU") : $(".menu_text").text("CLOSE");
+    });    
+
+	$(".menubtn").on("click", function() {
+		$(this).toggleClass('is_active');
+		$('.menu').slideToggle('fast');
 	});
 	
-	var $animation_elements = $('.-js_anime');
-	var $window = $(window);
 	
-	function check_if_in_view() {
-		var window_height = $window.height();
-		var window_top_position = $window.scrollTop();
-		var window_bottom_position = (window_top_position + window_height);
+	
+	$('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	  
+	  if (target.length) {
+        event.preventDefault();
+        
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('html, body').animate({
+          scrollTop: target.offset().top-100
+        }, 3000);
+		} else {
+			$('html, body').animate({
+          scrollTop: target.offset().top-130
+        }, 3000);
+	    }
+		  return false;
+        }
+      }
+    });
+    
+    
+    
+    
+    var page_url = window.location.href;
+	var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+	if ("page_id") {
+	    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$("html, body").animate({scrollTop: $("#" + page_id).offset().top-100}, 3000);
+		} else {
+			$("html, body").animate({scrollTop: $("#" + page_id).offset().top-130}, 3000);
+	    }
+	    
+	};
 		
-		$.each($animation_elements, function() {
-			var $element = $(this);
-			var element_height = $element.outerHeight();
-			var element_top_position = $element.offset().top;
-			var element_bottom_position = (element_top_position + element_height);
-			
-			if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-				$element.addClass('in-view');
-			} else {
-				$element.removeClass('in-view');
-			}
-		});
-	}
-	
-	$window.on('scroll resize', check_if_in_view);
-	$window.trigger('scroll');
 });
+
